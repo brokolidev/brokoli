388e0bf7b40c3c18217f4c39824cd4edd59dc40f
Hyunseung Choi <brokoli@Hyunseungs-MacBook-Pro.local>
2020-04-02 23:43:40 +0900

    apply tailwindcss and change main page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {},
+        fontFamily: {
+            sans: ['"Noto Sans KR"', "sans-serif"]
+        }
+    },
+    variants: {},
+    plugins: []
+};
